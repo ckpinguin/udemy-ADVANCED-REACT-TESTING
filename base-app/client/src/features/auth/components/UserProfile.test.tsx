71b2c5708519db45fc850be56082960cea030862
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-05 18:58:10 +0200

    wrapping react router (v5)

--- base-app/client/src/features/auth/components/UserProfile.test.tsx
+++ base-app/client/src/features/auth/components/UserProfile.test.tsx
@@ -1,8 +1,18 @@
-import { render, screen } from "@testing-library/react"
-
+import { render, screen } from "../../../test-utils"
 import { UserProfile } from "./UserProfile"
 
+const testUser = {
+  email: "test@test.com",
+}
+
 it("should say hi to the user", () => {
+  render(<UserProfile />, {
+    preloadedState: { user: { userDetails: testUser } },
+  })
+  expect(screen.getByText(/hi, test@test.com/i)).toBeInTheDocument()
+})
+
+it("should redirect if user is falsy", () => {
   render(<UserProfile />)
-  expect(screen.getByText(/hi/i)).toBeInTheDocument()
+  expect(screen.queryByText(/hi/i)).not.toBeInTheDocument()
 })
--- base-app/client/src/test-utils/index.tsx
+++ base-app/client/src/test-utils/index.tsx
@@ -7,19 +7,36 @@ import { Provider } from "react-redux"
 
 import { configureStoreWithMiddlewares, RootState } from "../app/store"
 import { ReactElement } from "react"
+import { createMemoryHistory } from "history"
+import { Router } from "react-router"
 
 type CustomRenderOptions = {
   preloadedState?: RootState
+  routeHistory?: Array<string>
+  initialRouteIndex?: number
   renderOptions?: Omit<RenderOptions, "wrapper">
 }
 
 function render(
   ui: ReactElement,
-  { preloadedState = {}, ...renderOptions }: CustomRenderOptions = {}
+  {
+    preloadedState = {},
+    routeHistory,
+    initialRouteIndex,
+    ...renderOptions
+  }: CustomRenderOptions = {}
 ): RenderResult {
   const Wrapper: React.FC = ({ children }) => {
     const store = configureStoreWithMiddlewares(preloadedState)
-    return <Provider store={store}>{children}</Provider>
+    const history = createMemoryHistory({
+      initialEntries: routeHistory,
+      initialIndex: initialRouteIndex,
+    })
+    return (
+      <Provider store={store}>
+        <Router history={history}>{children}</Router>
+      </Provider>
+    )
   }
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions })
 }
