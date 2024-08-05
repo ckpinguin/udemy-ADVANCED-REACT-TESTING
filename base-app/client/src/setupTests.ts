519899aee0655357229e07c5df68950d247d41d2
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-05 21:58:18 +0200

    introducing msw

--- base-app/client/src/setupTests.ts
+++ base-app/client/src/setupTests.ts
@@ -2,10 +2,12 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom/extend-expect"
+import { server } from "./mocks/server"
 
 beforeAll(() => {
   // https://github.com/nickcolley/jest-axe/issues/147#issuecomment-758804533
-  const { getComputedStyle } = window;
-  window.getComputedStyle = (elt) => getComputedStyle(elt);
-});
+  const { getComputedStyle } = window
+  window.getComputedStyle = (elt) => getComputedStyle(elt)
+  server.listen() // msw
+})
