54f67443ea292d5ad4e8c887aac30c9560f968e5
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-05 16:34:28 +0200

    first test (not working yet)

--- base-app/client/src/features/auth/components/UserProfile.test.tsx
+++ base-app/client/src/features/auth/components/UserProfile.test.tsx
@@ -1,1 +1,8 @@
 import { render, screen } from "@testing-library/react"
+
+import { UserProfile } from "./UserProfile"
+
+it("should say hi to the user", () => {
+  render(<UserProfile />)
+  expect(screen.getByText(/hi/i)).toBeInTheDocument()
+})
