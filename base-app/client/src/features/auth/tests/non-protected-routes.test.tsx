8245e48828a714110f715223d58300060911e6cf
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-06 20:33:43 +0200

    test.each() for unprotected routes

---
+++ base-app/client/src/features/auth/tests/non-protected-routes.test.tsx
@@ -0,0 +1,26 @@
+import { App } from "../../../App"
+import { render, screen } from "../../../test-utils"
+
+test.each([
+  { routeName: "Home", routePath: "/", headingMatch: /welcome/i },
+  {
+    routeName: "Band 1",
+    routePath: "/bands/1",
+    headingMatch: /joyous/i,
+  },
+  {
+    routeName: "Shows",
+    routePath: "/shows",
+    headingMatch: /shows/i,
+  },
+])(
+  "$routeName does not redirect to login screen",
+  async ({ routePath, headingMatch }) => {
+    render(<App />, { routeHistory: [routePath] }) // no route / history needed
+
+    const heading = await screen.findByRole("heading", {
+      name: headingMatch,
+    })
+    expect(heading).toBeInTheDocument()
+  }
+)
