68835bd9905a6717afde1fee564989e34a5b9ace
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-07 13:28:41 +0200

    different auth flows tests

---
+++ base-app/client/src/features/auth/tests/protected-routes.test.tsx
@@ -0,0 +1,88 @@
+import userEvent from "@testing-library/user-event"
+import { App } from "../../../App"
+import { render, getByRole, screen, waitFor } from "../../../test-utils"
+import { baseUrl, endpoints } from "../../../app/axios/constants"
+import { server } from "../../../mocks/server"
+import { handlers } from "../../../mocks/handlers"
+import {
+  DefaultRequestBody,
+  RequestParams,
+  ResponseComposition,
+  rest,
+  RestContext,
+  RestRequest,
+} from "msw"
+import { sign } from "crypto"
+
+test.each([
+  { routeName: "Profile", routePath: "/profile" },
+  { routeName: "Ticket 0", routePath: "/tickets/0" },
+  { routeName: "Confirm 0", routePath: "/confirm/0" },
+])("$routeName does redirect to login screen", ({ routePath }) => {
+  const { history } = render(<App />, { routeHistory: [routePath] })
+
+  const header = screen.getByRole("heading", { name: /sign/i })
+  expect(header).toBeInTheDocument()
+  expect(history.location.pathname).toBe("/signin")
+})
+
+test.each([
+  { testName: "sign in", buttonName: /sign in/i },
+  { testName: "sign up", buttonName: /sign up/i },
+])("successful $testName flow", async ({ buttonName }) => {
+  const { history } = render(<App />, { routeHistory: ["/tickets/1"] })
+
+  // sign up (after redirect)
+  const emailField = screen.getByLabelText(/email/i)
+  userEvent.type(emailField, "test@test.com")
+
+  const passwordField = screen.getByLabelText(/password/i)
+  userEvent.type(passwordField, "pas1234")
+
+  const signInForm = screen.getByTestId("sign-in-form") // workaround having more than 1 sign in buttons
+  const signInBtn = getByRole(signInForm, "button", { name: buttonName })
+  userEvent.click(signInBtn)
+
+  // redirect back to protected page
+  await waitFor(() => {
+    expect(history.location.pathname).toBe("/tickets/1")
+    expect(history.entries).toHaveLength(1)
+  })
+})
+
+const signInFailure = (
+  req: RestRequest<DefaultRequestBody, RequestParams>,
+  res: ResponseComposition<any>,
+  ctx: RestContext
+) => res(ctx.status(401))
+
+test("unsuccessful signin followed by successful signin", async () => {
+  const errorHandler = rest.post(
+    `${baseUrl}/${endpoints.signIn}`,
+    signInFailure
+  )
+  server.resetHandlers(...handlers, errorHandler)
+
+  const { history } = render(<App />, { routeHistory: ["/tickets/1"] })
+
+  const emailField = screen.getByLabelText(/email/i)
+  userEvent.type(emailField, "booking@test.com")
+
+  const passwordField = screen.getByLabelText(/password/i)
+  userEvent.type(passwordField, "cheese123")
+
+  const signInFOrm = screen.getByTestId("sign-in-form")
+  const signInBtn = getByRole(signInFOrm, "button", { name: /sign in/i })
+  userEvent.click(signInBtn)
+
+  // Reset to standard OK mock responses
+  server.resetHandlers()
+
+  // Try to login again, this time it should be successful
+  userEvent.click(signInBtn)
+
+  await waitFor(() => {
+    expect(history.location.pathname).toBe("/tickets/1")
+    expect(history.entries).toHaveLength(1)
+  })
+})
--- base-app/client/src/mocks/handlers.js
+++ base-app/client/src/mocks/handlers.js
@@ -3,6 +3,20 @@ import { rest } from "msw"
 import { showsUrl } from "../features/tickets/redux/showApi"
 import { bandUrl } from "../features/band/redux/bandApi"
 import { shows, bands } from "../test-utils/fake-data"
+import { baseUrl, endpoints } from "../app/axios/constants"
+
+const authHandler = (req, res, ctx) => {
+  const { email } = req.body
+  return res(
+    ctx.json({
+      user: {
+        id: 123,
+        email,
+        token: "abc123",
+      },
+    })
+  )
+}
 
 export const handlers = [
   rest.get(showsUrl, (req, res, ctx) => {
@@ -20,4 +34,6 @@ export const handlers = [
   rest.patch(`${showsUrl}/:showId/hold/:holdId`, (req, res, ctx) => {
     return res(ctx.status(200))
   }),
+  rest.post(`${baseUrl}/${endpoints.signIn}`, authHandler),
+  rest.post(`${baseUrl}/${endpoints.signUp}`, authHandler),
 ]
